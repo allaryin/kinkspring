e537d99d008916f922d4b6137878b896cb3adf76
Ammon Lauritzen <allaryin@gmail.com>
2013-09-23 20:44:28 -0500

    Added basic tooltip to the spring.

--- src/com/allaryin/kinkspring/item/ItemSpring.java
+++ src/com/allaryin/kinkspring/item/ItemSpring.java
@@ -1,6 +1,12 @@
 package com.allaryin.kinkspring.item;
 
+import java.util.List;
+
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemSpring extends KItem {
 
@@ -10,4 +16,11 @@ public class ItemSpring extends KItem {
 		setMaxStackSize(1);
 	}
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List info,
+			boolean useExtraInfo) {
+		info.add("An unwound spring.");
+	}
 }
